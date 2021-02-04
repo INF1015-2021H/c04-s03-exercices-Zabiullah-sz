f2fc23fe98e9d38df5f44ee2a800eea295ed4d3b
Polymtl-INF1015 <77556729+Polymtl-INF1015@users.noreply.github.com>
2021-02-04 16:29:13 -0500

    Auto sync with template repo

--- Exercises/_version_prof/main.cpp
+++ Exercises/_version_prof/main.cpp
@@ -13,53 +13,71 @@
 #include <vector>
 #include <span>
 
-#include <tclap/CmdLine.h>
 #include <cppitertools/range.hpp>
+#include <cppitertools/enumerate.hpp>
+#include <cppitertools/zip.hpp>
 
-#include "competition.hpp"
-#include "params.hpp"
-#include "pointers.hpp"
+#include "Employee.hpp"
+#include "Company.hpp"
+#include "tuples.hpp"
 
 using namespace std;
+using namespace iter;
 
 
-const int maxSize = 50;
-
-struct Computer {
-	string format, compagnie; double tailleEcranPouces, cpuGHz;
-	int memoireRamGiB, memoireDisqueGB;
-};
-
-struct ComputerList {
-	Computer elems[maxSize];
-	int      numElems = 0;
-};
-
-void tableauVsStruct(Computer tableau[maxSize], ComputerList& structure)
-{
-	tableau[0].cpuGHz = 2.2; // modifie le tableau original
-	structure.elems[0].cpuGHz = 2.2; // modifie la copie de la structure
+void runEmployeeExample() {
+	Employee marcel1 = Employee("Marcel1", 50'000);
+	Employee marcel2("Marcel2", 50'000);
+	Employee marcel3{"Marcel3"};
+	Employee marcel4 = Employee(); // Erreur si ctor par défaut pas défini
+	Employee marcel5 = {}; // Erreur si ctor par défaut pas défini
+	Employee* marcel6 = new Employee("Marcel6", 69);
+	marcel5.setSalary(1'000'000.0);
+	delete marcel6;
+	cout << marcel1.getName() << " " << marcel1.getSalary() << "\n"
+	     << marcel2.getName() << " " << marcel2.getSalary() << "\n"
+	     << marcel3.getName() << " " << marcel3.getSalary() << "\n"
+	     << marcel4.getName() << " " << marcel4.getSalary() << "\n"
+	     << marcel5.getName() << " " << marcel5.getSalary() << "\n";
+	cout << "- - - - - - - - - - -" << "\n";
 }
 
+void runCompanyExample() {
+	Company stonks("Stonks LLC", "chosson");
+	stonks.getPresident().setSalary(69'420);
+	//const Company stonks2("Stonks Jr LLC", "chosson jr");
+	//stonks2.getPresident().setSalary(69);
+	stonks.addEmployee("Foo", 69);
+	stonks.addEmployee("Bar", 42);
+	stonks.addEmployee("Qux", 9000);
+	stonks.addEmployee("Spam", 1337);
+	stonks.addEmployee("Eggs", 0xDEAD);
+	//stonks.print();
+	cout << "- - - - - - - - - - -" << "\n";
+}
+
+void runTuplesExample() {
+	{
+		vector<int> foo = {1, 3, -5, 10, 42, 69};
+		MinMax minmax = findMinMax_v1(foo);
+		auto&& [min, max] = findMinMax(foo);
+		auto minmax2 = findMinMax(foo);
+		cout << "min=" << minmax.min << ", max=" << minmax.max << "\n"
+			 << "min=" << min << ", max=" << max << "\n";
+	}
+	{
+		vector<int> bar = {1, 2, 3, 4, 5, 6};
+		auto&& [min, max, avg] = findMinMaxAvg(bar);
+		cout << "min=" << min << ", max=" << max << ", avg=" << avg << "\n";
+	}
+}
+
+
 int main() {
-	ComputerList foo = {};
-	Computer bar[maxSize] = {};
-	tableauVsStruct(bar, foo);
-	cout << foo.elems[0].cpuGHz << " " << bar[0].cpuGHz << "\n";
-
-	runParamsExample();
-	cout << string(50, '-') << "\n";
-
-	cout << "Competition example:" << "\n";
-	runCompetitionExample();
-	cout << string(50, '-') << "\n";
-	
-	cout << "1D dynamic arrays example:" << "\n";
-	run1DDynamicArrayExample();
-	cout << string(50, '-') << "\n";
-	
-	cout << "2D dynamic arrays example:" << "\n";
-	run2DDynamicArrayExample();
-	cout << string(50, '-') << "\n";
+	runEmployeeExample();
+	cout << "\n\n";
+	runCompanyExample();
+	cout << "\n\n";
+	runTuplesExample();
 }
 
